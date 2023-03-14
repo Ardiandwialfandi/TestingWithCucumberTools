9360a4b08b5bd870e067be8d77fc60e0336c4dbc
Ardian Dwi Alfandi <dwiardian25@gmail.com>
2023-03-14 11:36:52 +0700

    Ardian FirstCommit

--- src/test/java/com/juaracoding/cucumber/foldertest/TestAddtoCart.java
+++ src/test/java/com/juaracoding/cucumber/foldertest/TestAddtoCart.java
@@ -4,6 +4,8 @@ import com.juaracoding.cucumber.lib.LibraryTest;
 import com.juaracoding.cucumber.pages.AddtoCartPage;
 import com.juaracoding.cucumber.pages.LoginDemoqaPage;
 import com.juaracoding.cucumber.utils.RegularPage;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,18 +15,19 @@ import org.testng.Assert;
 public class TestAddtoCart {
     public static WebDriver driver;
     public static AddtoCartPage addCart;
-
+    public static ExtentTest extentTest;
     public static LoginDemoqaPage logDem;
 
     public TestAddtoCart() {
         driver = LibraryTest.driver;
         addCart = new AddtoCartPage();
         logDem = new LoginDemoqaPage();
+        extentTest = LibraryTest.extentTest;
     }
 
     @When("User scroll and select color")
     public void user_scroll_and_select_color() {
-        System.out.println("User scroll and select color");
+        extentTest.log(LogStatus.PASS,"User scroll and select color");
         RegularPage.scrollPage("0", "620");
         RegularPage.delayDuration(2);
         addCart.selectColor();
@@ -32,26 +35,26 @@ public class TestAddtoCart {
 
     @When("User click button view cart")
     public void User_click_button_view_cart() {
-        System.out.println("User click button view cart");
+        extentTest.log(LogStatus.PASS,"User click button view cart");
         addCart.btnView();
     }
 
     @And("User login")
     public void user_login() {
-        System.out.println("User login");
+        extentTest.log(LogStatus.PASS,"User login");
         logDem.enterLogin("Ardiandwi", "Ard14&','//");
     }
 
     @And("User click link text checkout")
     public void user_click_link_text_checkout() {
-        System.out.println("User click link text checkout");
+        extentTest.log(LogStatus.PASS,"User click link text checkout");
         RegularPage.delayDuration(1);
         addCart.clickCekot();
     }
 
     @And("User scroll and click button return to shop")
     public void user_scroll_and_click_button_return_to_shop() {
-        System.out.println("User scroll and click button return to shop");
+        extentTest.log(LogStatus.PASS,"User scroll and click button return to shop");
         RegularPage.scrollPage("0", "300");
         RegularPage.delayDuration(1);
         addCart.btnReturnShop();
@@ -59,7 +62,7 @@ public class TestAddtoCart {
 
     @And("User scroll and click product one")
     public void user_scroll_and_click_product_one() {
-        System.out.println("User scroll and click product one");
+        extentTest.log(LogStatus.PASS,"User scroll and click product one");
         RegularPage.scrollPage("0", "600");
         addCart.chooseProductOne();
         RegularPage.delayDuration(2);
@@ -67,13 +70,13 @@ public class TestAddtoCart {
 
     @And("User click exit for quit product one")
     public void user_click_exit_for_quit_product_one() {
-        System.out.println("User click exit for quit product one");
+        extentTest.log(LogStatus.PASS,"User click exit for quit product one");
         addCart.exitProductOne();
     }
 
     @And("User scroll and click product two")
     public void user_scroll_and_click_product_two() {
-        System.out.println("User scroll and click product two");
+        extentTest.log(LogStatus.PASS,"User scroll and click product two");
         RegularPage.delayDuration(1);
         RegularPage.scrollPage("0", "500");
         addCart.chooseProductTwo();
@@ -81,59 +84,60 @@ public class TestAddtoCart {
 
     @And("User click button select product")
     public void user_click_button_select_product() {
-        System.out.println("User click button select product");
+        extentTest.log(LogStatus.PASS,"User click button select product");
         RegularPage.delayDuration(2);
         addCart.btnSelectProduct();
     }
 
     @And("User select size")
     public void user_select_size() {
-        System.out.println("User select size");
+        extentTest.log(LogStatus.PASS,"User select size");
         RegularPage.delayDuration(1);
         addCart.selectSize();
     }
 
     @And("User click button increament")
     public void user_click_button_increament() {
-        System.out.println("User click button increament");
+        extentTest.log(LogStatus.PASS,"User click button increament");
         RegularPage.delayDuration(2);
         addCart.btnIncr();
     }
 
     @And("User click button decreament")
     public void user_click_button_decreament() {
-        System.out.println("User click button decreament");
+        extentTest.log(LogStatus.PASS,"User click button decreament");
         addCart.btnDecr();
     }
 
     @And("User click button add to cart")
     public void user_click_button_add_to_cart() {
-        System.out.println("User click button add to cart");
+        extentTest.log(LogStatus.PASS,"User click button add to cart");
         addCart.btnAddtoCart();
     }
 
     @And("User scroll clear product")
     public void User_scroll_clear_product() {
+        extentTest.log(LogStatus.PASS,"User scroll clear prodduct");
         RegularPage.scrollPage("0", "600");
         addCart.clear();
     }
 
     @Then("User go to page and get name product")
     public void user_go_to_page_and_get_name_product() {
-        System.out.println("User go to page and get name product");
+        extentTest.log(LogStatus.PASS,"User go to page and get name product");
         Assert.assertTrue(addCart.getNameProduct().contains("BLACK STRAPPY"));
     }
 
     @Then("User get to confirm product")
     public void user_get_to_confirm_product() {
-        System.out.println("User get to confirm product");
+        extentTest.log(LogStatus.PASS,"User get to confirm product");
         Assert.assertTrue(addCart.getConfirmProduct().contains("black strappy"));
     }
 
     @Then("User go to page cart")
     public void user_go_to_page_cart() {
-        System.out.println("User go to page cart");
-        Assert.assertTrue(addCart.getReturnShop().contains("Cart"));
+        extentTest.log(LogStatus.PASS,"User go to page cart");
+        Assert.assertTrue(addCart.getReturnShop().contains("Cartt"));
     }
 }
 
--- src/test/java/com/juaracoding/cucumber/foldertest/TestLogin.java
+++ src/test/java/com/juaracoding/cucumber/foldertest/TestLogin.java
@@ -4,6 +4,8 @@ import com.juaracoding.cucumber.lib.LibraryTest;
 import com.juaracoding.cucumber.pages.LoginDemoqaPage;
 import com.juaracoding.cucumber.utils.Regular;
 import com.juaracoding.cucumber.utils.RegularPage;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -14,16 +16,19 @@ import org.testng.Assert;
 public class TestLogin {
 
     public static WebDriver driver;
+
+    public static ExtentTest extentTest;
     public static LoginDemoqaPage logDemoq;
 
     public TestLogin() {
         driver = LibraryTest.driver;
         logDemoq = new LoginDemoqaPage();
+        extentTest = LibraryTest.extentTest;
     }
 
     @Given("User open browser and url")
     public void user_open_browser_and_url() {
-        System.out.println("User open browser and url");
+        extentTest.log(LogStatus.PASS,"User open browser and url");
         driver.get(Regular.URL);
         Assert.assertTrue(logDemoq.getTitleBar().contains("ToolsQA"));
         RegularPage.delayDuration(1);
@@ -31,21 +36,21 @@ public class TestLogin {
 
     @Given("User logout web demoqa shop")
     public void User_logout_web_demoqa_shop(){
-        System.out.println("User logout web demoqa shop");
+        extentTest.log(LogStatus.PASS,"User logout web demoqa shop");
         RegularPage.delayDuration(1);
         logDemoq.linktxLogout();
     }
 
     @When("User click popup dismiss")
     public void user_click_popup_dismiss() {
-        System.out.println("User click popup dismiss");
+        extentTest.log(LogStatus.PASS,"User click popup dismiss");
         logDemoq.clickPopUp();
         RegularPage.delayDuration(1);
     }
 
     @When("User scroll and enter invalid username")
     public void user_scroll_and_enter_invalid_username(){
-        System.out.println("User scroll and enter invalid username");
+        extentTest.log(LogStatus.PASS,"User scroll and enter invalid username");
         RegularPage.scrollPage("0","500");
         RegularPage.delayDuration(1);
         logDemoq.enterUsernama("Ardian");
@@ -53,57 +58,57 @@ public class TestLogin {
 
     @And("User click link my account and scroll")
     public void user_click_link_my_account_and_scroll() {
-        System.out.println("User click link my account and scroll");
+        extentTest.log(LogStatus.PASS,"User click link my account and scroll");
         logDemoq.activityLogin();
         RegularPage.scrollPage("0", "500");
     }
 
     @And("User enter valid username")
     public void user_enter_valid_username() {
-        System.out.println("User enter valid username");
+        extentTest.log(LogStatus.PASS,"User enter valid username");
         logDemoq.enterUsernama("Ardiandwi");
     }
 
     @And("User enter valid password")
     public void user_enter_valid_password() {
-        System.out.println("User enter valid password");
+        extentTest.log(LogStatus.PASS,"User enter valid password");
         logDemoq.enterPassword("Ard14&','//");
     }
 
     @And("User click checkbox remember me")
     public void user_click_checkbox_remember_me() {
-        System.out.println("User click checkbox remember me");
+        extentTest.log(LogStatus.PASS,"User click checkbox remember me");
         logDemoq.clickRememberme();
     }
 
     @And("User click button login")
     public void user_click_button_login() {
-        System.out.println("User click button login");
+        extentTest.log(LogStatus.PASS,"User click button login");
         logDemoq.clickBtnLogin();
     }
 
     @And("User go to page my account and scroll")
     public void user_go_to_page_my_account_and_scroll() {
-        System.out.println("User go to page my account");
+        extentTest.log(LogStatus.PASS,"User go to page my account");
         logDemoq.getTextMyacc();
         RegularPage.scrollPage("0", "400");
     }
 
     @And("User enter invalid password")
     public void user_enter_invalid_password(){
-        System.out.println("User enter invalid password");
+        extentTest.log(LogStatus.PASS,"User enter invalid password");
         logDemoq.enterPassword("juara");
     }
 
     @Then("User get account name")
     public void user_get_account_name() {
-        System.out.println("User get account name");
+        extentTest.log(LogStatus.PASS,"User get account name");
         Assert.assertEquals(logDemoq.getAccountName(), "Ardiandwi");
     }
 
     @Then("User get message error message")
     public void user_get_message_error_message(){
-        System.out.println("User get message error message");
+        extentTest.log(LogStatus.PASS,"User get message error message");
         RegularPage.scrollPage("0","400");
         RegularPage.delayDuration(1);
         Assert.assertTrue(logDemoq.messageError().contains("ERROR:"));
